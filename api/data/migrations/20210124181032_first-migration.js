8ca67d58fb8f26e2f0931eeb32e5161d637a3c43
Anwar <AnwarAldogmi@gmail.com>
2021-09-21 17:14:57 -0700

    wip

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,25 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
+    .createTable('roles', (roles) => {
+      roles.increments('role_id')
+      roles.string('role_name', 32).notNullable().unique()
+    })
+    .createTable('users', users => {
       users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
+      users.string('username', 128).notNullable().unique()
+      users.string('password', 128).notNullable()
+      users.integer('role_id')
+        .unsigned()
+        .notNullable()
+        .references('role_id')
+        .inTable('roles')
+        .onUpdate('RESTRICT')
+        .onDelete('RESTRICT')
     })
 }
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
+  await knex.schema
+  .dropTableIfExists('users')
+  .dropTableIfExists('roles')
 }
---
+++ api/users/users-router.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const router = express.Router()
+
+const Users = require('./users-model')
+
+const { restricted, only } = require('../auth/auth-middleware')
+
+router.get("/", restricted, (req, res, next) => {
+    Users.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(next)
+})
+
+router.get("/:user_id", restricted, only('admin'), (req, res, next) => {
+    Users.findById(req.params.user_id)
+    .then(user => {
+        res.json(user);
+    })
+    .catch(next);
+});
+
+module.exports = {
+    router
+}
